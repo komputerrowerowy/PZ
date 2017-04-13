e9cfa99b575c1d9baa8e059cd66e143467841b20
Daniel <danieljaskolski768@gmail.com>
2017-04-13 18:52:25 +0200

    Update list skrolowanych

--- aktualnaWersja/screen/main.py
+++ aktualnaWersja/screen/main.py
@@ -1133,7 +1133,7 @@ class CallScreen(Screen):
 
                     btn.color = (0.235, 0.529, 0.572, 1)
                     btn.background_normal = ''
-                    btn1.background_down = ''
+                    btn.background_down = ''
                     btn.background_color = (1, 1, 1, 1)
                     btn.size_hint_x = 0.82
                 else:
@@ -1146,7 +1146,7 @@ class CallScreen(Screen):
 
                     btn.color = (0.235, 0.529, 0.572, 1)
                     btn.background_normal = ''
-                    btn1.background_down = ''
+                    btn.background_down = ''
                     btn.background_color = (.941, .960, .960, 1)
                     btn.size_hint_x = 0.82
 
